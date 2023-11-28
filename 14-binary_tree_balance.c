c816ddeb95b5a920d09c54c59644762b7f0b7b95
Aaron Pius <piuschbz@gmail.com>
2023-11-28 12:47:53 -0800

    checked leaf node while checking balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -35,13 +35,19 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left, right;
+	int left = 0, right = 0;
 
 	if (tree)
 	{
-		left = binary_tree_height(tree->left);
-		right = binary_tree_height(tree->right);
+		if (tree->left && (!tree->left->left && !tree->left->right))
+			left = 1;
+		else
+			left = binary_tree_height(tree->left);
+		if (tree->right && (!tree->right->right && !tree->right->left))
+			right = 1;
+		else
+			right = binary_tree_height(tree->right);
 		return (left - right);
 	}
-	return (0);
+	return (left - right);
 }
